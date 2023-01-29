37de2605c7b4093a0bce448bda51b16239f7a6ae
Ashutosh Singh <ashutoshsi@prod.hclpnp.com>
2023-01-29 12:00:19 +0530

    Added migration to add new columns to booking table

--- src/models/booking.js
+++ src/models/booking.js
@@ -27,6 +27,16 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: "InProcess",
       values: ["InProcess", "Booked", "Cancelled"],
       allowNull: false
+    },
+    noOfseats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    totalCost: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     }
   }, {
     sequelize,
